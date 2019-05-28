23da5fd5360101fa5e27fbc9dfdafd067664ed3f
Ximena sofia <ximena.pocco@ucsp.edu.pe>
2019-05-28 17:08:13 -0400

    remove heap

--- MaxHeap.py
+++ MaxHeap.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import math
 
-myH = [5,12,64,1,37,90,91,97]
+#myH = [90,60,40,12,3,15,18]
+myH = [5,12,64,1,90,37,91,97]
+#myH = [5,12,64,1,37,90,91,97]
+
 
 def getLeftSon(pos):
     value = pos * 2 + 1
@@ -43,6 +46,17 @@ def insertHeap(elemt):
     if getLeftSon(i2)[0] < getRightSon(i2)[0]:
         return  myH
     return maxHeap(i2)
+    
+#Solo remover el root, pues como el Heap es una cola de prioridad y  el root tiene el mayor valor, entonces no se necesita remover otro que no sea el root 
+def remove():
+    if len(myH) == 1:
+        del myH[0]
+        return 
+    #Swap con el ultimo elemento del array
+    myH[0], myH[ len(myH)-1] = myH[ len(myH)-1] , myH[0]
+    del myH[ len(myH)-1 ]
+    i = math.ceil(len(myH)/2)-1    
+    maxHeap(i)
                
 if __name__ == "__main__":
     i = math.ceil(len(myH)/2)-1 #start position
